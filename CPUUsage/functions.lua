96abb122bea4cd9ea6bb9830aeeac1b7f9aa4879
Turbstructor <ycw0201@naver.com>
2020-12-23 10:34:10 +0900

    Added Example screenshot; Changed font of numbers in Clock skin; Changes implemented in CPUUsage Mk.2 skin, VideoMemoryUsage/VirtualMemoryUsage skins; Min function modified in functions.lua; Removed unnecessary fonts

--- CPUUsage/functions.lua
+++ CPUUsage/functions.lua
@@ -7,3 +7,15 @@ function maxN(...)
     table.sort(valueTable, function(a, b) return a > b end)
     return valueTable[1]
 end
+
+function minN(...)
+    valueTable = {}
+    for i = 1, #arg do
+        if(arg[i] > 0 or arg[i] ~= nil) then
+            table.insert(valueTable, arg[i])
+        end
+    end
+
+    table.sort(valueTable, function(a, b) return a < b end)
+    return valueTable[1]
+end
